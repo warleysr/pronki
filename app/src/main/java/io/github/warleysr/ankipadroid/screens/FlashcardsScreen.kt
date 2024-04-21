fbf794b975a7c5b4ed87394570ec4d8159991b8d
Warley <warleysoaresr@gmail.com>
2024-04-21 19:55:54 -0300

    fix result words display

--- app/src/main/java/io/github/warleysr/ankipadroid/screens/FlashcardsScreen.kt
+++ app/src/main/java/io/github/warleysr/ankipadroid/screens/FlashcardsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -25,13 +26,20 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.github.warleysr.ankipadroid.AnkiDroidHelper
 import io.github.warleysr.ankipadroid.R
+import io.github.warleysr.ankipadroid.viewmodels.AnkiDroidViewModel
 import io.github.warleysr.ankipadroid.viewmodels.PronunciationViewModel
 import io.github.warleysr.ankipadroid.viewmodels.SettingsViewModel
 import kotlinx.coroutines.launch
 
 @Composable
-fun FlashcardsScreen(settingsViewModel: SettingsViewModel, pronunciationViewModel: PronunciationViewModel) {
+fun FlashcardsScreen(
+    settingsViewModel: SettingsViewModel,
+    pronunciationViewModel: PronunciationViewModel,
+    ankiDroidViewModel: AnkiDroidViewModel
+) {
+
     val coroutineScope = rememberCoroutineScope()
     var success by remember { mutableStateOf(false) }
     var currentStatus by remember { mutableStateOf("Click here") }
@@ -39,68 +47,76 @@ fun FlashcardsScreen(settingsViewModel: SettingsViewModel, pronunciationViewMode
     var performing by remember { mutableStateOf(false) }
     var recording by remember { mutableStateOf(false) }
 
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.TopCenter
     ) {
-        if (!success) {
-            Column {
+        var decks = ankiDroidViewModel.getDeckList()
 
-                OutlinedTextField(
-                    currentInput,
-                    label = { Text("Text to pronounce") },
-                    onValueChange = {
-                        currentInput = it
-                    },
-                    enabled = !performing
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Row (
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Button(
-                        enabled = !performing,
-                        onClick = {
-                            coroutineScope.launch {
-                                performing = true
-                                val azureKey = settingsViewModel.getSetting("azure_key")
-                                val language = settingsViewModel.getSetting("language")
-                                val region = settingsViewModel.getSetting("region")
-
-                                pronunciationViewModel.newAssessment(
-                                    currentInput,
-                                    language = language,
-                                    speechApiKey = azureKey,
-                                    speechRegion = region,
-                                    onResult = { result ->
-                                        success = result
-                                        currentStatus = if (result) "OK" else "Canceled"
-                                        performing = false
-                                    }
-                                )
-                            }
-                        }
-                    ) {
-                        Text(text = currentStatus)
-                    }
-                    Button (
-                        enabled = !performing,
-                        onClick = {
-                            recording = !recording
-                            if (recording) {
-                                pronunciationViewModel.startRecording()
-                            }
-                            else {
-                                pronunciationViewModel.stopRecording()
-                            }
-                        }
-                    ) {
-                        Text(if (recording) "Stop" else "Record")
-                    }
-                }
+        decks?.forEach { deck ->
+            Row {
+                Text(deck)
             }
-        } else {
-            PronunciationAssessmentResults(pronunciationViewModel)
         }
+//        if (!success) {
+//            Column {
+//
+//                OutlinedTextField(
+//                    currentInput,
+//                    label = { Text("Text to pronounce") },
+//                    onValueChange = {
+//                        currentInput = it
+//                    },
+//                    enabled = !performing
+//                )
+//                Spacer(modifier = Modifier.height(8.dp))
+//                Row (
+//                    modifier = Modifier.padding(16.dp)
+//                ) {
+//                    Button(
+//                        enabled = !performing,
+//                        onClick = {
+//                            coroutineScope.launch {
+//                                performing = true
+//                                val azureKey = settingsViewModel.getSetting("azure_key")
+//                                val language = settingsViewModel.getSetting("language")
+//                                val region = settingsViewModel.getSetting("region")
+//
+//                                pronunciationViewModel.newAssessment(
+//                                    currentInput,
+//                                    language = language,
+//                                    speechApiKey = azureKey,
+//                                    speechRegion = region,
+//                                    onResult = { result ->
+//                                        success = result
+//                                        currentStatus = if (result) "OK" else "Canceled"
+//                                        performing = false
+//                                    }
+//                                )
+//                            }
+//                        }
+//                    ) {
+//                        Text(text = currentStatus)
+//                    }
+//                    Button (
+//                        enabled = !performing,
+//                        onClick = {
+//                            recording = !recording
+//                            if (recording) {
+//                                pronunciationViewModel.startRecording()
+//                            }
+//                            else {
+//                                pronunciationViewModel.stopRecording()
+//                            }
+//                        }
+//                    ) {
+//                        Text(if (recording) "Stop" else "Record")
+//                    }
+//                }
+//            }
+//        } else {
+//            PronunciationAssessmentResults(pronunciationViewModel)
+//        }
     }
 }
