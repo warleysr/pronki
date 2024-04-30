978bd28a6766e7cefe2f0a0f221bfaf2aaf55696
Warley Soares <warley.soares@enacom.com.br>
2024-04-29 21:04:54 -0300

    add animation and progress indicator to record FAB

--- app/src/main/java/io/github/warleysr/ankipadroid/screens/RecordFAB.kt
+++ app/src/main/java/io/github/warleysr/ankipadroid/screens/RecordFAB.kt
@@ -1,11 +1,14 @@
 package io.github.warleysr.ankipadroid.screens
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
@@ -15,22 +18,28 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.FlipToBack
 import androidx.compose.material.icons.filled.FlipToFront
 import androidx.compose.material.icons.filled.Mic
-import androidx.compose.material.icons.outlined.Mic
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.unit.dp
 import io.github.warleysr.ankipadroid.viewmodels.AnkiDroidViewModel
 import io.github.warleysr.ankipadroid.viewmodels.PronunciationViewModel
 import io.github.warleysr.ankipadroid.viewmodels.SettingsViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun RecordFAB(
@@ -41,41 +50,65 @@ fun RecordFAB(
     onExit: () -> Unit
 ) {
     val interactionSource = remember { MutableInteractionSource() }
-    val isPressed by interactionSource.collectIsPressedAsState()
+    var recording by remember { mutableStateOf(false) }
+    var cancelled by remember { mutableStateOf(false) }
     var performing by remember { mutableStateOf(false) }
     var useFront by remember { mutableStateOf(true) }
 
-    if (isPressed) {
-        if (performing) return
-        println("Pressed! Recording voice...")
-        pronunciationViewModel.startRecording()
 
-        DisposableEffect(Unit) {
-            onDispose {
-                println("Released! Starting assessment...")
-                pronunciationViewModel.stopRecording()
+    val viewConfiguration = LocalViewConfiguration.current
 
-                val referenceText = if (useFront) ankiDroidViewModel.cardInfo!!.question else ankiDroidViewModel.cardInfo!!.answer
-                val azureKey = settingsViewModel.getSetting("azure_key")
-                val language = settingsViewModel.getSetting("language")
-                val region = settingsViewModel.getSetting("region")
+    LaunchedEffect(interactionSource) {
 
-                pronunciationViewModel.newAssessment(
-                    referenceText = referenceText!!,
-                    language = language,
-                    speechApiKey = azureKey,
-                    speechRegion = region,
-                    onResult = { result ->
-                        performing = false
-                    }
-                )
+        interactionSource.interactions.collectLatest { interaction ->
+            when (interaction) {
+                is PressInteraction.Press -> {
+                    delay(viewConfiguration.longPressTimeoutMillis)
+                    println("Recording...")
+                    recording = true
+
+                    pronunciationViewModel.startRecording()
+                }
+
+                is PressInteraction.Release -> {
+                    println("Released! Starting assessment...")
+                    pronunciationViewModel.stopRecording()
+
+                    recording = false
+                    performing = true
+
+                    val referenceText = if (useFront) ankiDroidViewModel.cardInfo!!.question else ankiDroidViewModel.cardInfo!!.answer
+                    val azureKey = settingsViewModel.getSetting("azure_key")
+                    val language = settingsViewModel.getSetting("language")
+                    val region = settingsViewModel.getSetting("region")
+
+                    pronunciationViewModel.newAssessment(
+                        referenceText = referenceText!!,
+                        language = language,
+                        speechApiKey = azureKey,
+                        speechRegion = region,
+                        onResult = { result ->
+                            performing = false
+                        }
+                    )
+                }
+
+                is PressInteraction.Cancel -> {
+                    println("Cancelled...")
+                    pronunciationViewModel.stopRecording()
+                    recording = false
+                    cancelled = true
+                }
             }
         }
     }
+
     Column(
         horizontalAlignment = Alignment.End,
         verticalArrangement = Arrangement.Bottom,
-        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
     ) {
         Row {
             SmallFloatingActionButton(
@@ -103,12 +136,34 @@ fun RecordFAB(
             }
             Spacer(Modifier.width(8.dp))
 
+            val sizeState = animateDpAsState(
+                targetValue = if (recording) 84.dp else 56.dp,
+                animationSpec = tween(durationMillis = 300, delayMillis = 5),
+                label = ""
+            )
+
             FloatingActionButton(
                 shape = CircleShape,
                 onClick = { },
                 interactionSource = interactionSource,
+                containerColor = if (recording) Color.LightGray else FloatingActionButtonDefaults.containerColor,
+                modifier = Modifier
+                    .defaultMinSize(
+                        minWidth = sizeState.value,
+                        minHeight = sizeState.value
+                    )
             ) {
-                Icon(if (isPressed) Icons.Filled.Mic else Icons.Outlined.Mic, null)
+                if (performing) {
+                    CircularProgressIndicator(strokeWidth = 2.dp)
+                } else {
+                    Icon(
+                        Icons.Filled.Mic, null,
+                        modifier = Modifier.defaultMinSize(
+                            minWidth = sizeState.value.div(2),
+                            minHeight = sizeState.value.div(2)
+                        )
+                    )
+                }
             }
         }
     }
