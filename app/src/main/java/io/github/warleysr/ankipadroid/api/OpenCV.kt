5bde1e1e6d891b4b5aa1afb7cd8f1d64832ed165
Warley <warleysoaresr@gmail.com>
2024-05-01 17:59:26 -0300

    trying image threshold and bounding boxes

--- app/src/main/java/io/github/warleysr/ankipadroid/api/OpenCV.kt
+++ app/src/main/java/io/github/warleysr/ankipadroid/api/OpenCV.kt
@@ -6,6 +6,8 @@ import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import org.opencv.android.Utils
 import org.opencv.core.Mat
+import org.opencv.core.Point
+import org.opencv.core.Scalar
 import org.opencv.imgproc.Imgproc
 
 class OpenCV {
@@ -16,19 +18,43 @@ class OpenCV {
 
             val originalMat = Mat()
             Utils.bitmapToMat(bitmap, originalMat)
+
             val tempMat = Mat()
+            var tempMat2 = Mat()
             Imgproc.cvtColor(originalMat, tempMat, Imgproc.COLOR_BGR2GRAY)
-            Utils.matToBitmap(tempMat, bitmap)
+//            Imgproc.threshold(tempMat, tempMat2, 0.0, 255.0, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU)
+            tempMat2 = tempMat
+
+            val tempBitmap = Bitmap.createBitmap(bitmap)
+            Utils.matToBitmap(tempMat2, tempBitmap)
 
             val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-            val image = InputImage.fromBitmap(bitmap, rotation)
+            val image = InputImage.fromBitmap(tempBitmap, rotation)
 
             val result = recognizer.process(image)
-                .addOnSuccessListener { visionText ->
-                    onSuccess(visionText.text)
+
+            result.addOnSuccessListener { visionText ->
+                visionText.textBlocks.forEach { textBlock ->
+                    textBlock.lines.forEach { line ->
+                        line.elements.forEach { elem ->
+                            val box = elem.boundingBox!!
+                            Imgproc.rectangle(
+                                originalMat,
+                                Point(box.left.toDouble(), box.top.toDouble()),
+                                Point(box.right.toDouble(), box.bottom.toDouble()),
+                                Scalar(255.0, 0.0, 1.0, 255.0),
+                                2
+                            )
+                        }
+                    }
                 }
-                .addOnFailureListener { e ->
-                }
+
+                Utils.matToBitmap(originalMat, bitmap)
+
+                onSuccess(visionText.text)
+            }
+            .addOnFailureListener { e ->
+            }
 
         }
     }
