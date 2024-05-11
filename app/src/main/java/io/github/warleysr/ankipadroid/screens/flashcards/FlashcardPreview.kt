d6164493897e1887d7c26adc6d4f45f1587c3a5e
Warley Soares <warley.soares@enacom.com.br>
2024-05-10 21:55:21 -0300

    feat: add snackbar and parse html for card preview

--- app/src/main/java/io/github/warleysr/ankipadroid/screens/flashcards/FlashcardPreview.kt
+++ app/src/main/java/io/github/warleysr/ankipadroid/screens/flashcards/FlashcardPreview.kt
@@ -1,5 +1,11 @@
 package io.github.warleysr.ankipadroid.screens.flashcards
 
+import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,30 +27,72 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.core.text.parseAsHtml
 import io.github.warleysr.ankipadroid.AnkiDroidHelper
 import io.github.warleysr.ankipadroid.R
 import io.github.warleysr.ankipadroid.viewmodels.AnkiDroidViewModel
 import io.github.warleysr.ankipadroid.viewmodels.PronunciationViewModel
 import io.github.warleysr.ankipadroid.viewmodels.SettingsViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FlashcardPreview(
     settingsViewModel: SettingsViewModel,
     pronunciationViewModel: PronunciationViewModel,
     ankiDroidViewModel: AnkiDroidViewModel
+) {
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        content = {
+            FlashcardPreviewContent(
+                settingsViewModel = settingsViewModel,
+                pronunciationViewModel = pronunciationViewModel,
+                ankiDroidViewModel = ankiDroidViewModel,
+                snackbarHostState = snackbarHostState,
+                scope = scope
+            )
+        }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FlashcardPreviewContent(
+    settingsViewModel: SettingsViewModel,
+    pronunciationViewModel: PronunciationViewModel,
+    ankiDroidViewModel: AnkiDroidViewModel,
+    snackbarHostState: SnackbarHostState,
+    scope: CoroutineScope
 ) {
     var currentQuestion by remember { mutableStateOf("Waiting...") }
     var currentAnswer by remember { mutableStateOf("Waiting...") }
@@ -55,7 +103,6 @@ fun FlashcardPreview(
         currentQuestion = question
         currentAnswer = answer
     }
-
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.TopCenter
@@ -84,7 +131,9 @@ fun FlashcardPreview(
                             .verticalScroll(rememberScrollState())
                             .weight(1f, false)
                     ) {
-                        Text(currentQuestion, style = MaterialTheme.typography.bodyLarge,
+                        Text(
+                            currentQuestion.parseAsHtml().toAnnotatedString(),
+                            style = MaterialTheme.typography.bodyLarge,
                             modifier = Modifier.padding(16.dp),
                             textAlign = TextAlign.Center,
                         )
@@ -113,7 +162,8 @@ fun FlashcardPreview(
                                     .weight(1f, false)
                             ) {
                                 Text(
-                                    currentAnswer, style = MaterialTheme.typography.bodyLarge,
+                                    currentAnswer.parseAsHtml().toAnnotatedString(),
+                                    style = MaterialTheme.typography.bodyLarge,
                                     modifier = Modifier.padding(16.dp),
                                     textAlign = TextAlign.Center,
                                 )
@@ -193,7 +243,12 @@ fun FlashcardPreview(
                 RecordFAB(
                     settingsViewModel, pronunciationViewModel, ankiDroidViewModel,
                     onBackUse = { showAnswer = true },
-                    onExit = { deckSelected = false }
+                    onExit = { deckSelected = false },
+                    onFailure = { reason ->
+                        scope.launch {
+                            snackbarHostState.showSnackbar(reason)
+                        }
+                    }
                 )
             } else {
                 Text(stringResource(id = R.string.select_deck), style = MaterialTheme.typography.headlineMedium)
@@ -207,6 +262,37 @@ fun FlashcardPreview(
                     }
                 }
             }
+        }
+    }
+}
+
+fun Spanned.toAnnotatedString(): AnnotatedString = buildAnnotatedString {
+    val spanned = this@toAnnotatedString
+    append(spanned.toString())
+    spanned.getSpans(0, spanned.length, Any::class.java).forEach { span ->
+        val start = spanned.getSpanStart(span)
+        val end = spanned.getSpanEnd(span)
+        when (span) {
+            is StyleSpan -> when (span.style) {
+                Typeface.BOLD -> addStyle(SpanStyle(fontWeight = FontWeight.Bold), start, end)
+                Typeface.ITALIC -> addStyle(SpanStyle(fontStyle = FontStyle.Italic), start, end)
+                Typeface.BOLD_ITALIC -> addStyle(
+                    SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic
+                    ), start, end
+                )
+            }
+            is UnderlineSpan -> addStyle(
+                SpanStyle(textDecoration = TextDecoration.Underline),
+                start,
+                end
+            )
+            is ForegroundColorSpan -> addStyle(
+                SpanStyle(color = Color(span.foregroundColor)),
+                start,
+                end
+            )
         }
     }
 }
