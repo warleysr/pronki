42ae51e31d7f74c67a765cff401c85f7af845c89
Warley <warleysoaresr@gmail.com>
2024-07-29 22:42:04 -0300

    fix: selecting deck before querying next scheduled card

--- app/src/main/java/io/github/warleysr/ankipadroid/api/ankidroid/AnkiDroidAPI.kt
+++ app/src/main/java/io/github/warleysr/ankipadroid/api/ankidroid/AnkiDroidAPI.kt
@@ -1,5 +1,6 @@
 package io.github.warleysr.ankipadroid.api.ankidroid
 
+import android.content.ContentValues
 import android.net.Uri
 import com.ichi2.anki.FlashCardsContract
 import com.ichi2.anki.api.AddContentApi
@@ -8,54 +9,60 @@ import io.github.warleysr.ankipadroid.AnkiPADroid
 class AnkiDroidAPI {
 
     companion object {
-    fun queryNextCard(deck: DeckInfo): CardInfo? {
-        println("Selected deck: ${deck.deckName} - ${deck.deckId}")
 
-        val builder = FlashCardsContract.ReviewInfo.CONTENT_URI.buildUpon()
-            .appendQueryParameter(
-                "deckID", deck.deckId.toString()
+        private val CARD_PROJECTION = arrayOf(
+            FlashCardsContract.Card.QUESTION_SIMPLE, FlashCardsContract.Card.ANSWER_SIMPLE
+        )
+
+        fun queryNextCard(deck: DeckInfo): CardInfo? {
+            println("Selected deck: ${deck.deckName} - ${deck.deckId}")
+
+            val values = ContentValues()
+            values.put(FlashCardsContract.Deck.DECK_ID, deck.deckId)
+            AnkiPADroid.instance.applicationContext.contentResolver.update(
+                FlashCardsContract.Deck.CONTENT_SELECTED_URI, values, null, null
             )
-            .appendQueryParameter("limit", "5")
-        val deckUri = builder.build()
-        val deckCursor = AnkiPADroid.instance.applicationContext.contentResolver.query(
-            deckUri, null, null, null, null
-        )
-        if (deckCursor == null || !deckCursor.moveToFirst())
-            return null
 
-        println("Deck Uri: $deckUri")
+            val deckUri = FlashCardsContract.ReviewInfo.CONTENT_URI
+            val deckCursor = AnkiPADroid.instance.applicationContext.contentResolver.query(
+                deckUri, null, null, null, null
+            )
+            if (deckCursor == null || !deckCursor.moveToFirst())
+                return null
 
-        val noteIdCol = deckCursor.getColumnIndex(FlashCardsContract.ReviewInfo.NOTE_ID)
-        val cardOrdCol = deckCursor.getColumnIndex(FlashCardsContract.ReviewInfo.CARD_ORD)
-        val noteId = deckCursor.getLong(noteIdCol)
-        val cardOrd = deckCursor.getInt(cardOrdCol)
+            println("Deck Uri: $deckUri")
 
-        deckCursor.close()
+            val noteIdCol = deckCursor.getColumnIndex(FlashCardsContract.ReviewInfo.NOTE_ID)
+            val cardOrdCol = deckCursor.getColumnIndex(FlashCardsContract.ReviewInfo.CARD_ORD)
+            val noteId = deckCursor.getLong(noteIdCol)
+            val cardOrd = deckCursor.getInt(cardOrdCol)
 
-        val noteUri = Uri.withAppendedPath(
-            FlashCardsContract.Note.CONTENT_URI, noteId.toString()
-        )
-        val cardsUri = Uri.withAppendedPath(noteUri, "cards")
-        val specificCardUri = Uri.withAppendedPath(cardsUri, cardOrd.toString())
+            deckCursor.close()
 
-        val cardCursor = AnkiPADroid.instance.applicationContext.contentResolver.query(
-            specificCardUri, null, null, null, null
-        )
-        if (cardCursor == null || !cardCursor.moveToFirst())
-            return null
+            val noteUri = Uri.withAppendedPath(
+                FlashCardsContract.Note.CONTENT_URI, noteId.toString()
+            )
+            val cardsUri = Uri.withAppendedPath(noteUri, "cards")
+            val specificCardUri = Uri.withAppendedPath(cardsUri, cardOrd.toString())
+
+            val cardCursor = AnkiPADroid.instance.applicationContext.contentResolver.query(
+                specificCardUri, CARD_PROJECTION, null, null, null
+            )
+            if (cardCursor == null || !cardCursor.moveToFirst())
+                return null
 
 
-        println("Specific card Uri: $specificCardUri")
-        val questionCol = cardCursor.getColumnIndex(FlashCardsContract.Card.QUESTION)
-        val answerCol = cardCursor.getColumnIndex(FlashCardsContract.Card.ANSWER)
+            println("Specific card Uri: $specificCardUri")
+            val questionCol = cardCursor.getColumnIndex(FlashCardsContract.Card.QUESTION_SIMPLE)
+            val answerCol = cardCursor.getColumnIndex(FlashCardsContract.Card.ANSWER_SIMPLE)
 
-        val question = cardCursor.getString(questionCol)
-        val answer = cardCursor.getString(answerCol)
+            val question = cardCursor.getString(questionCol)
+            val answer = cardCursor.getString(answerCol)
 
-        cardCursor.close()
+            cardCursor.close()
 
-        return CardInfo(noteId, cardOrd, question, answer)
-    }
+            return CardInfo(noteId, cardOrd, question, answer)
+        }
 
         fun getDeckList(): List<DeckInfo>? {
             return AddContentApi(AnkiPADroid.instance.applicationContext)
