368927f801bc1aafd12cc6f9dd0b6fb777e600a8
Warley <warleysoaresr@gmail.com>
2024-07-29 22:04:02 -0300

    bump minSdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.warleysr.ankipadroid"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
