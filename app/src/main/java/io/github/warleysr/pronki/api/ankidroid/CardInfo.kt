b6de8897015a20c0efdc92d34fac1a6676c33901
Warley <warleysoaresr@gmail.com>
2024-12-02 11:35:24 -0300

    process question and answer on card info instance

--- app/src/main/java/io/github/warleysr/pronki/api/ankidroid/CardInfo.kt
+++ app/src/main/java/io/github/warleysr/pronki/api/ankidroid/CardInfo.kt
@@ -3,7 +3,10 @@ package io.github.warleysr.pronki.api.ankidroid
 data class CardInfo(
     val noteId: Long,
     val cardOrd: Int,
-    val question: String,
-    val answer: String,
+    val questionRaw: String,
+    val answerRaw: String,
     val startTime: Long = System.currentTimeMillis()
-)
+) {
+    val question: String = questionRaw.replace("\\[anki:.*?]".toRegex(), "")
+    val answer: String = answerRaw.replace("\\[anki:.*?]".toRegex(), "")
+}
