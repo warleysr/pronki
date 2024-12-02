8b1bdbac9482540b6cd4f7e325af9e64625515e3
Warley <warleysoaresr@gmail.com>
2024-12-02 11:10:29 -0300

    update libraries and sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "io.github.warleysr.pronki"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.warleysr.pronki"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
